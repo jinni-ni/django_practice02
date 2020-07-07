8a052d065791775c47c3fb0231a67f2938db7aed
김수진 <sjkim910115@gmail.com>
2020-07-07 19:41:09 +0900

    2020.07.07

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id','title','modify_dt')
